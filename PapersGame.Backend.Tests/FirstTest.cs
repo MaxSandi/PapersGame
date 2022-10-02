8eafcaa23267e8fa394204d16ff9c476a00cc384
Maxim Schekolda <maxsandi.work@gmail.com>
2022-10-02 14:52:12 +0300

    added project for PapersGame.Backend tests

---
+++ PapersGame.Backend.Tests/FirstTest.cs
@@ -0,0 +1,11 @@
+﻿namespace PapersGame.Backend.Tests
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void FirstTest_ShouldReturnTrue_Always()
+        {
+            Assert.True(true);
+        }
+    }
+}
